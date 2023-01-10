0f7849d727b750b6838e93710d827e719a4374e3
Neto Sama <jnetogorio@gmail.com>
2023-01-10 11:04:26 -0300

    Desafio finalizado

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,8 +3,17 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            this.Numero = numero;
+            this.Modelo = modelo;
+            this.IMEI = imei;
+            this.Memoria = memoria;
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
-        public override void InstalarAplicativo(string nomeApp) {
+        public override void InstalarAplicativo(string nomeApp)
+        {
             Console.WriteLine($"Aplicativo {nomeApp} instalado no iPhone");
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia(numero: "35648125", modelo: "N95", imei: "52421542154", memoria: 256);
 nokia.Ligar();
 nokia.InstalarAplicativo("Wild Rift");
 
+Console.WriteLine("Smartphone iPhone:");
 Smartphone iphone = new Iphone(numero: "985467851", modelo: "XS", imei: "124578454126", memoria: 128);
-nokia.ReceberLigacao();
-nokia.InstalarAplicativo("Tower of Fantasy");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tower of Fantasy");
