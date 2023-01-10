fdf0c3e8728792268ccf030470af85e6f21c1223
ayesama <85299067+ayesama@users.noreply.github.com>
2023-01-10 10:54:29 -0300

    Implementações do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "35648125", modelo: "N95", imei: "52421542154", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Wild Rift");
+
+Smartphone iphone = new Iphone(numero: "985467851", modelo: "XS", imei: "124578454126", memoria: 128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Tower of Fantasy");
